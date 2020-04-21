929ea0671326456f519539f6816be02ef4a67997
larissa <lari.barabasz@gmail.com>
2020-04-21 00:51:38 -0300

    feat(global-error-handler): custom global error handler.

---
+++ src/app/core/services/global-error-handler/global-error-handler.service.ts
@@ -0,0 +1,20 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { LoggerService } from '../../../shared/services/logger-service/logger.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalErrorHandlerService  implements ErrorHandler {
+
+  constructor(private injector: Injector) { }
+
+  public handleError(err: any): void {
+    if (environment.log.enabled) {
+      const logService = this.injector.get(LoggerService);
+      logService.critical(err.originalError || err);
+    }
+
+    throw err;
+  }
+}
