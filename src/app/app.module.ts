5a80cb0797ad4be82d351acf058cc3b43e3636d0
larissa <lari.barabasz@gmail.com>
2020-04-21 00:55:23 -0300

    feat(app.module): integrate with GlobalErrorHandlerService, ApiInterceptor and TranslateModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,53 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { ErrorHandler, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ExampleCoreService } from './core/example-service/example-core.service';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+// ====================================
+// SERVICES
+// ====================================
+
+import { ExampleCoreService } from "./core/services/example-service/example-core.service";
+import { GlobalErrorHandlerService } from "./core/services/global-error-handler/global-error-handler.service";
+
+// ====================================
+// INTERCEPTORS
+// ====================================
+
+import { ApiInterceptor } from "./core/interceptors/api/api.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot()
+  ],
   providers: [
     ExampleCoreService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    },
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandlerService
+    }
   ],
   bootstrap: [AppComponent],
 })
