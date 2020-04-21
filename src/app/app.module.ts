8a2b2f195902c26df635066606e768f34c0e2d10
larissa <lari.barabasz@gmail.com>
2020-04-21 15:22:24 -0300

    feat(custom-translation-loader): integrate with app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { ErrorHandler, NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouteReuseStrategy } from "@angular/router";
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
-import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
-import { SplashScreen } from "@ionic-native/splash-screen/ngx";
-import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 // ====================================
 // SERVICES
 // ====================================
 
-import { ExampleCoreService } from "./core/services/example-service/example-core.service";
-import { GlobalErrorHandlerService } from "./core/services/global-error-handler/global-error-handler.service";
+import { ExampleCoreService } from './core/services/example-service/example-core.service';
+import { GlobalErrorHandlerService } from './core/services/global-error-handler/global-error-handler.service';
+import { CustomTranslationsLoaderService } from './core/services/custom-translations-loader-service/custom-translations-loader.service';
 
 // ====================================
 // INTERCEPTORS
 // ====================================
 
-import { ApiInterceptor } from "./core/interceptors/api/api.interceptor";
+import { ApiInterceptor } from './core/interceptors/api/api.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,7 +33,13 @@ import { ApiInterceptor } from "./core/interceptors/api/api.interceptor";
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    TranslateModule.forRoot()
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useClass: CustomTranslationsLoaderService,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     ExampleCoreService,
@@ -42,12 +49,12 @@ import { ApiInterceptor } from "./core/interceptors/api/api.interceptor";
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
-      multi: true
+      multi: true,
     },
     {
       provide: ErrorHandler,
-      useClass: GlobalErrorHandlerService
-    }
+      useClass: GlobalErrorHandlerService,
+    },
   ],
   bootstrap: [AppComponent],
 })
