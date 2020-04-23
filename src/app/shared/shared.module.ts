4ff5f2e100addb3548d74b3c9bbb8848ed8d9bdb
larissa <lari.barabasz@gmail.com>
2020-04-23 03:14:20 -0300

    feat(shared.module): import shared form components.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { SharedExampleComponent } from './components/shared-example/shared-example.component';
 
+// ================================================================================================
+// FORM COMPONENTS
+// ================================================================================================
+
+import { AppFieldPasswordComponent } from './components/app-field-password/app-field-password.component';
+import { AppFieldEmailComponent } from './components/app-field-email/app-field-email.component';
+import { AppFieldErrorMessageComponent } from './components/app-field-error-message/app-field-error-message.component';
+import { AppFieldNumberComponent } from './components/app-field-number/app-field-number.component';
+import { AppFieldTextComponent } from './components/app-field-text/app-field-text.component';
+
 @NgModule({
-  declarations: [SharedExampleComponent],
-  imports: [CommonModule, TranslateModule.forChild({})],
-  exports: [SharedExampleComponent, TranslateModule],
+  declarations: [
+    SharedExampleComponent,
+    AppFieldErrorMessageComponent,
+    AppFieldPasswordComponent,
+    AppFieldEmailComponent,
+    AppFieldNumberComponent,
+    AppFieldTextComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule, TranslateModule.forChild({})],
+  exports: [
+    SharedExampleComponent,
+    TranslateModule,
+    AppFieldErrorMessageComponent,
+    AppFieldPasswordComponent,
+    AppFieldEmailComponent,
+    AppFieldNumberComponent,
+  ],
 })
 export class SharedModule {}
