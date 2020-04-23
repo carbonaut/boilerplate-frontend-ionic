544e3c25caf6ce59d38759cdd4538a756391f51e
Felipe Tadra <felipetadra@gmail.com>
2020-04-23 14:34:43 -0300

    feat(button): create button component
    Refers to #22

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() size: 'large' | 'small' | '';
+  @Input('clear') isClear: boolean;
+  @Input('outline') isOutline: boolean;
+  @Input('inverted') isInverted: boolean;
+  @Input('arrow') hasArrow: boolean;
+
+  @Input() icon: string;
+  @Input() type: boolean;
+  @Input() loading: boolean;
+  @Input() disabled: boolean;
+
+  @Output() click = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClick($event) {
+    this.click.emit($event);
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { SharedExampleComponent } from './components/shared-example/shared-example.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [SharedExampleComponent],
-  imports: [CommonModule, TranslateModule.forChild({})],
-  exports: [SharedExampleComponent, TranslateModule],
+  declarations: [SharedExampleComponent, ButtonComponent],
+  imports: [CommonModule, IonicModule.forRoot(), TranslateModule.forChild({})],
+  exports: [TranslateModule, SharedExampleComponent, ButtonComponent],
 })
 export class SharedModule {}
