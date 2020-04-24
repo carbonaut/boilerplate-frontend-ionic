ed7e76ff484759f070fb53d35e085874dd8d158a
larissa <lari.barabasz@gmail.com>
2020-04-23 21:02:33 -0300

    feat(field-radio): throw error when theme is toggle and total options are different from 2.

--- src/app/shared/components/field-radio/field-radio.component.ts
+++ src/app/shared/components/field-radio/field-radio.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FieldRadioOption } from './field-radio.interface';
 
+const TOTAL_OPTIONS_TOGGLE_THEME = 2;
+
 @Component({
   selector: 'app-field-radio',
   templateUrl: './field-radio.component.html',
@@ -17,6 +19,8 @@ export class FieldRadioComponent implements OnInit {
   @Input() showValidationErrorMessage = true;
 
   ngOnInit() {
+    this.validateThemeField();
+
     if (!this.control.value) {
       this.selectDefaultOption();
     }
@@ -24,6 +28,12 @@ export class FieldRadioComponent implements OnInit {
 
   trackByOptions(index: number, option: FieldRadioOption) {
     return option.value;
+  }
+
+  private validateThemeField() {
+    if (this.theme === 'toggle' && this.options.length !== TOTAL_OPTIONS_TOGGLE_THEME) {
+      throw new Error('Theme "toggle" requires 2 options.');
+    }
   }
 
   private selectDefaultOption() {
