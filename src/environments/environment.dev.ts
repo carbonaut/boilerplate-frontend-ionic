8f751c7b600e40a4c79ae8f2a58c5da6bda851eb
Luiz F. Vasconcellos <luizvasconcellos53@gmail.com>
2022-03-21 16:10:07 -0300

    build(configuration): add dev environment file

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  api: {
+    endpoint: '',
+    maxWaiting: 10000,
+    authHeader: 'Authorization',
+  },
+  log: {
+    enabled: false,
+    payload: {},
+  },
+  availableLanguages: ['en', 'de'],
+};
