54628a1173c5583c833bec24d5c6ed33b6a31768
Melissa Fernandes <melfer100@gmail.com>
2022-08-24 15:22:40 -0300

    feat: add electron basic code

---
+++ electron.ts
@@ -0,0 +1,41 @@
+// imports must be require
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/www/index.html`),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
