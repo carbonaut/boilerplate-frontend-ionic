4812e94ff6e87a4fb3dd9f415b2177f0c6b96052
Melissa Fernandes <melfer100@gmail.com>
2022-08-26 16:29:04 -0300

    refactor: example store init on home page

--- src/app/modules/example-feature/components/presentation/presentation.component.ts
+++ src/app/modules/example-feature/components/presentation/presentation.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ExamplesService } from '../../state/examples.service';
-import { ExampleRepository } from '../../state/examples.repository';
 
 @Component({
   selector: 'app-presentation',
@@ -8,18 +6,5 @@ import { ExampleRepository } from '../../state/examples.repository';
   styleUrls: ['./presentation.component.scss'],
 })
 export class PresentationComponent implements OnInit {
-  constructor(private examplesRepository: ExampleRepository, private exampleService: ExamplesService) {
-    // Just an example for elf repository usage
-    // A presentational component shouldn't be concerned
-    // with data fetching logic.
-    this.exampleService.loadExamples().subscribe();
-
-    // You can also bound this observable to a variable
-    // and use it with an async pipe on template
-    this.examplesRepository.examples$.subscribe((examples) => {
-      console.log(examples);
-    });
-  }
-
   ngOnInit() {}
 }
