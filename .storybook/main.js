16d36b8560191b579820e20ad39df6f125385fcd
Melissa Fernandes <melfer100@gmail.com>
2022-08-30 11:01:08 -0300

    chore: setup storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/angular",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/angular',
+  core: {
+    builder: '@storybook/builder-webpack5',
+    disableTelemetry: true,
+    enableCrashReports: true,
+  },
+};
